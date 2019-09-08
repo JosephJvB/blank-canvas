15e045087bc2742d8b74f64e7bd0b8f12f0f31d6
JosephJvB <joevanbo@pm.me>
2019-09-08 13:42:04 +1200

    ripples grow and reset

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 console.log('====\n\n script loaded \n\n====');
+
+const C = document.querySelector('canvas');
+const ctx = C.getContext('2d');
+const rec = C.getBoundingClientRect();
+
+function clicker () {
+    ripp = new Ripple();
+    anni();
+}
+
+function anni () {
+    ctx.clearRect(0,0,rec.width, rec.height);
+    ripp.draw();
+    requestAnimationFrame(anni);
+}
+
+function Ripple () {
+
+    let state = {};
+
+    const init = () => {
+        console.log('rip init');
+        state.r = Math.random() * (30 - 5) + 5;
+        state.x = Math.random() * 300;
+        state.y = Math.random() * 150;
+        state.max = Math.random() * (100 - 25) + 25;
+        state.v = Math.random() * (0.9 - 0.4) + 0.4;
+    }
+
+    // constructor
+    (() => {
+        init();
+    })();
+
+
+    this.draw = () => {
+        // if circle gets too big, start again
+        if(state.r >= state.max) {
+            init();
+        }
+        state.r += state.v;
+        ctx.beginPath();
+        ctx.arc(state.x, state.y, state.r, 0, 2 * Math.PI, false);
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+    }
+}
--- readme.md
+++ readme.md
@@ -2,6 +2,14 @@
 
 Wanna write some beginner canvas code
 
+Copying: https://github.com/nodeschool/wellington/tree/master/website
+
+What's my first goal?
+1. Circle goes from one side of the canvas to the other
+    1. Get circle on canvas
+    2. make circle move??
+    
 Inspired by
 - @ahdinosaur's https://play.gridbeam.xyz
-- Nodeschool Wellington's website: https://nodeschool.io/wellington/, https://github.com/nodeschool/wellington/tree/master/website
+- Nodeschool Wellington's website: https://nodeschool.io/wellington/
+- Not canvas, but some very cool stuff https://xkcd.com/1608/
