25f1c95c425807ad247369966869b88367d8b59c
JosephJvB <joevanbo@pm.me>
2019-09-09 22:46:11 +1200

    disable bad meme

--- static/counter.js
+++ static/counter.js
@@ -18,9 +18,9 @@ function Counter () {
     ctx.font = '100 48px SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace';
     ctx.fillStyle = 'black';
     ctx.fillText(n, x, y);
-    if(n.toString().includes(69)) {
-      ctx.font = '100 12px SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace';
-      ctx.fillText('nice', x + digitOffset, y + 20);
-    }
+    // if(n.toString().includes(69)) {
+    //   ctx.font = '100 12px SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace';
+    //   ctx.fillText('nice', x + digitOffset, y + 20);
+    // }
   }
 }
